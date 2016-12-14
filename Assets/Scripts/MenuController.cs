84cdfb303b0718ccd7cac6ab66bcc645fdfe378b
mikel <mikelros22@gmail.com>
2016-12-14 22:08:22 +0100

    added initial menu, ball texture, pause and resume. Polish and stuff.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Play()
+    {
+        SceneManager.LoadScene("main");
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,12 @@ public class PlayerController : MonoBehaviour {
     
     public float speed;
     public Text scoreText;
-    public Text winText;
-    public int lifes;
+    public Text livesText;
+    public Text finalText;
+    public Text pauseText;
+    public int lives;
+    public Camera pauseCamera;
+    public Camera playCamera;
 
     private Rigidbody rb;
     private int score;
@@ -15,41 +19,46 @@ public class PlayerController : MonoBehaviour {
     {
         rb = GetComponent<Rigidbody>();
         score = 0;
-        ChangeText();
-        winText.text = "";
+        ChangeScoreText();
+        ChangeLivesText();
+        finalText.text = "";
     }
 
     void FixedUpdate()
     {
-        Vector3 movement;
-        if (Application.isMobilePlatform)
+        if(transform.position.y >= 0)
         {
-            movement = new Vector3(Input.acceleration.x, 0, Input.acceleration.y);
-        } else
-        {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement;
+            if (Application.isMobilePlatform)
+            {
+                movement = new Vector3(Input.acceleration.x, 0, Input.acceleration.y);
+            }
+            else
+            {
+                float moveHorizontal = Input.GetAxis("Horizontal");
+                float moveVertical = Input.GetAxis("Vertical");
 
-             movement = new Vector3(moveHorizontal, 0, moveVertical);
+                movement = new Vector3(moveHorizontal, 0, moveVertical);
+            }
+
+            movement = Camera.main.transform.TransformDirection(movement);
+            movement.y = 0;
+            rb.AddForce(movement * speed);
+
         }
-
-        movement = Camera.main.transform.TransformDirection(movement);
-        movement.y = 0;
-        rb.AddForce(movement * speed);
-
     }
 
     void Update()
     {
-        if (lifes < 0)
+        if(transform.position.y < -5)
         {
-            Destroy(this.gameObject);
-            winText.text = "YOU LOSE";
-        }
-        if(transform.position.y < -3)
-        {
-            lifes--;
+            lives--;
+            ChangeLivesText();
             transform.position = new Vector3(0, 0, 0);
+
+            //removes residual forces acquired while falling
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
         }
     }
 
@@ -58,17 +67,48 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("pickup"))
         {
             other.gameObject.SetActive(false);
+            speed += 2;
             score++;
-            ChangeText();
+            ChangeScoreText();
         }
     }
 
-    private void ChangeText()
+    private void ChangeScoreText()
     {
         scoreText.text = "Score: " + score;
         if(score >= 7)
         {
-            winText.text = "YOU WIN!!";
+            Destroy(this.gameObject);
+            finalText.text = "YOU WIN!!";
+        }
+    }
+
+    private void ChangeLivesText()
+    {
+        livesText.text = "Lives left: " + lives;
+        if (lives == 0)
+        {
+            Destroy(this.gameObject);
+            finalText.text = "YOU LOSE";
+        }
+    }
+
+    public void OnPauseAndResume()
+    {
+        
+        if(pauseText.text.Equals("Pause"))
+        {
+            pauseCamera.gameObject.SetActive(true);
+            playCamera.gameObject.SetActive(false);
+            pauseText.text = "Resume";
+            Time.timeScale = 0;
+            
+        } else
+        {
+            playCamera.gameObject.SetActive(true);
+            pauseCamera.gameObject.SetActive(false);
+            Time.timeScale = 1;
+            pauseText.text = "Pause";
         }
     }
 }
